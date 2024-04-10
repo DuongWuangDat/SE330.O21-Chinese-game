06dec565a69f7aae07c48e09047dfd4058c2a2f7
DQP-UIT <22521149@gm.uit.edu.vn>
2024-04-11 02:27:26 +0700

    DrawBoard

--- src/main/java/com/example/chinese_chess/screen/GameScreen.java
+++ src/main/java/com/example/chinese_chess/screen/GameScreen.java
@@ -1,6 +1,9 @@
 package com.example.chinese_chess.screen;
 
+import com.example.chinese_chess.model.Board;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class GameScreen extends Application {
@@ -11,6 +14,14 @@ public class GameScreen extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 1200, 800); // Đặt kích thước của cửa sổ ứng dụng
 
+        // Tạo đối tượng bảng cờ
+        Board board = new Board(root, primaryStage, scene);
+
+        primaryStage.setTitle("Chinese Chess");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
